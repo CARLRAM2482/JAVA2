b0e6b7d7bbda7574b77952cd2130c28689ab9235
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-16 11:47:10 -0500

    v1 v1.1

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,21 @@
 public class Principal {
     public static void main(String[] args) {
+        //Propio tipo de datos una nueva istancia
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre ="logan";
+        miPelicula.fechaDeLanzamiento = 2022;
+        miPelicula.duracionEnMinutos = 120;
+
+        System.out.println("mi pelicula es : " +miPelicula.nombre);
+        System.out.println("fecha de lanzamiento es : "+ miPelicula.fechaDeLanzamiento);
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Vengadores";
+        otraPelicula.fechaDeLanzamiento = 2000;
+        otraPelicula.duracionEnMinutos = 120;
+
+        System.out.println("mi otra pelicula es : " + otraPelicula.nombre);
+        System.out.println("fecha de lanzamiento es : " + otraPelicula.fechaDeLanzamiento);
 
     }
 }
