70ad564c25932399a1a628fa0bc55cc5ab77fe8c
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-27 08:48:54 -0500

    v1.3 geddes

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,13 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         //Propio tipo de datos una nueva istancia
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre ="logan";
-        miPelicula.fechaDeLanzamiento = 2022;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("logan");
+        miPelicula.setFechaDeLanzamiento(2022);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
        miPelicula.muestraFichaTecnica();
        miPelicula.evalua(7);
@@ -15,15 +18,19 @@ public class Principal {
         System.out.println(miPelicula.calculaMedia());
 
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Vengadores";
-        otraPelicula.fechaDeLanzamiento = 2000;
-        otraPelicula.duracionEnMinutos = 120;
 
-        otraPelicula.muestraFichaTecnica();
 
-        System.out.println("mi otra pelicula es : " + otraPelicula.nombre);
-        System.out.println("fecha de lanzamiento es : " + otraPelicula.fechaDeLanzamiento);
+
+
+   //     com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
+   //     otraPelicula.nombre = "Vengadores";
+   //     otraPelicula.fechaDeLanzamiento = 2000;
+  //      otraPelicula.duracionEnMinutos = 120;
+
+   //     otraPelicula.muestraFichaTecnica();
+
+   //     System.out.println("mi otra pelicula es : " + otraPelicula.nombre);
+   //     System.out.println("fecha de lanzamiento es : " + otraPelicula.fechaDeLanzamiento);
 
     }
 }
