977da036118ad264ec5ab8959dbe5ec753411813
CARLRAM2482 <145763670+CARLRAM2482@users.noreply.github.com>
2024-09-27 07:31:07 -0500

    v1.2

--- src/Principal.java
+++ src/Principal.java
@@ -6,13 +6,21 @@ public class Principal {
         miPelicula.fechaDeLanzamiento = 2022;
         miPelicula.duracionEnMinutos = 120;
 
-        System.out.println("mi pelicula es : " +miPelicula.nombre);
-        System.out.println("fecha de lanzamiento es : "+ miPelicula.fechaDeLanzamiento);
+       miPelicula.muestraFichaTecnica();
+       miPelicula.evalua(7);
+       miPelicula.evalua(9);
+       miPelicula.evalua(9);
+
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Vengadores";
         otraPelicula.fechaDeLanzamiento = 2000;
         otraPelicula.duracionEnMinutos = 120;
+
+        otraPelicula.muestraFichaTecnica();
 
         System.out.println("mi otra pelicula es : " + otraPelicula.nombre);
         System.out.println("fecha de lanzamiento es : " + otraPelicula.fechaDeLanzamiento);
