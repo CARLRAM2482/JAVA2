61865e8aa352259327303216d79f62e479add77b
Carlos M. Ramos <145763670+CARLRAM2482@users.noreply.github.com>
2024-10-22 18:22:42 -0500

    V5.0   persistiendo datos en archivos

--- src/com/aluracursos/principal/PrincipalConBusqueda.java
+++ src/com/aluracursos/principal/PrincipalConBusqueda.java
@@ -13,50 +13,68 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalConBusqueda {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Escribe el nombre de una pelicula:");
-        var busqueda = lectura.nextLine();
 
-        String direccion = "https://www.omdbapi.com/?t="+busqueda.replace(" ", "+")+"&apikey=7b7ebea2";
+        List<Titulo> titulos = new ArrayList<>();
 
+        Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
 
+        while (true){
+            System.out.println("Escribe el nombre de una pelicula:");
+            var busqueda = lectura.nextLine();
 
-        try {
-            HttpClient client = HttpClient.newHttpClient();
+            if (busqueda.equalsIgnoreCase("salir")){
+                break;
+            }
 
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create(direccion))
-                    .build();
-            HttpResponse<String> response = client
-                    .send(request, HttpResponse.BodyHandlers.ofString());
-
-            String json = response.body();
-            System.out.println(json);
-
-            Gson gson = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE).create();
-
-            TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-            System.out.println(miTituloOmdb);
-
-            Titulo miTitulo = new Titulo(miTituloOmdb);
-            System.out.println("Titulo ya comvertido"+miTitulo);
+            String direccion = "https://www.omdbapi.com/?t="+busqueda.replace(" ", "+")+"&apikey=7b7ebea2";
 
 
-            FileWriter escritura = new FileWriter("peliculas.txt");
-            escritura.write(miTitulo.toString());
-            escritura.close();
-        }catch (NumberFormatException e){
-            System.out.println("ocurio un error: ");
-            System.out.println(e.getMessage());
-        }catch (IllegalArgumentException e){
-            System.out.println("Error en la URI, verifique la direccion");
-        }catch (ErrorEnConvercionDeDuracionException e){
-            System.out.println(e.getMensaje());
+
+            try {
+                HttpClient client = HttpClient.newHttpClient();
+
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(direccion))
+                        .build();
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                String json = response.body();
+                System.out.println(json);
+
+
+
+                TituloOmdb miTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                System.out.println(miTituloOmdb);
+
+                Titulo miTitulo = new Titulo(miTituloOmdb);
+                System.out.println("Titulo ya comvertido"+miTitulo);
+
+
+                titulos.add(miTitulo);
+            }catch (NumberFormatException e){
+                System.out.println("ocurio un error: ");
+                System.out.println(e.getMessage());
+            }catch (IllegalArgumentException e){
+                System.out.println("Error en la URI, verifique la direccion");
+            }catch (ErrorEnConvercionDeDuracionException e){
+                System.out.println(e.getMensaje());
+            }
         }
+        System.out.println(titulos);
+
+        FileWriter escritura = new FileWriter("titulos.json");
+        escritura.write(gson.toJson(titulos));
+        escritura.close();
         System.out.println("finalizo la ejecucion del programa!");
 
 
